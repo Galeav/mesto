f46a5c8d7a8098f67e2f3ef3fc4da7b235b52080
Sergiy Borsakov <serg.universe@yandex.ru>
2021-04-20 14:25:02 +0300

    Beta 1.0

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+//Функционал для открытия/закрытия попапа
+let openPopup = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closePopup = document.querySelector('.popup__close');
+
+function popup_status(event) {
+  popup.classList.toggle('popup_opened');
+}
+openPopup.addEventListener('click', popup_status);
+closePopup.addEventListener('click', popup_status);
+
+//Функционал для изменения значения value у форм попапа
+let formPopup = document.querySelector('.popup__edit-form');
+let nameInput = document.getElementById('nameInput');
+let jobInput = document.getElementById('proffInput');
+let jobValue = '';
+let nameValue = '';
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  jobValue = jobInput.value;
+  nameValue = nameInput.value;
+  profileName.textContent = nameValue;
+  profileJob.textContent = jobValue;
+  popup_status();
+}
+formPopup.addEventListener('submit', formSubmitHandler);
