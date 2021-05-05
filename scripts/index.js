763651971153bea22157257883015021e6a2254c
Sergiy Borsakov <serg.universe@yandex.ru>
2021-05-05 09:24:52 +0300

    Добавлен функционал динамического заполнения карточек мест на страницу, возможность лайка карточки, добавлен новый попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
+//Функционал для попапа с изменением данных профиля
 //Функционал для открытия/закрытия попапа
 let openPopup = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
+let popupProfile = document.querySelector('.popup-profile');
 let closePopup = document.querySelector('.popup__close');
 
 function popupStatus(event) {
-  popup.classList.toggle('popup_opened');
+  popupProfile.classList.toggle('popup_opened');
   //Функционал заполняет поля для ввода данными из строк
-  let popupOp = popup.classList.contains('popup_opened');
+  let popupOp = popupProfile.classList.contains('popup_opened');
   if (popupOp) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
@@ -31,3 +32,107 @@ function formSubmitHandler(evt) {
 formPopup.addEventListener('submit', formSubmitHandler);
 openPopup.addEventListener('click', popupStatus);
 closePopup.addEventListener('click', popupStatus);
+
+//Функционал для попапа с добавлением картинок
+let openPlacePopup = document.querySelector('.profile__add-button');
+let popupPlace = document.querySelector('.popup-place');
+let closePlacePopup = document.querySelector('.popup-place__close');
+
+function popupPlaceStatus(event) {
+  popupPlace.classList.toggle('popup_opened');
+  testing(); //Функция отвечает за отклик лайка после добавления новых картинок
+}
+
+openPlacePopup.addEventListener('click', popupPlaceStatus);
+closePlacePopup.addEventListener('click', popupPlaceStatus);
+
+//функционал попапа с просмотром картинки
+
+//функционал автоматической загрузки картинок в начале на страницу
+const buttonAdd = document.getElementById('popupPlaceSubmitButton');   //Кнопка "создать" отправляет данные из формы с именем и ссылкой на картинку
+const inputNamePlace = document.getElementById('namePlace');        //Поле вводе наименования картинки
+const inputLinkPlace = document.getElementById('picLink');          //Поле ввода ссылки на картинку которая должна попасть в поле src в html
+const elementsSection = document.querySelector('.elements');         //Добавление секции в дом элемент, которая будет заполняться карточками
+const taskTemplate = document.getElementById('template');           //Шаблон карточки которая будет клонироваться
+
+//Массив с изменяющимеся данными карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+for (let i = 0; i < initialCards.length; i += 1) {
+  const namePlace = initialCards[i].name;
+  const linkPlace = initialCards[i].link;
+
+  const newTask = taskTemplate.content.querySelector('.elements__element').cloneNode('true');
+
+  const nameElement = newTask.querySelector('.elements__foto-name');
+  nameElement.textContent = namePlace;
+  const linkElement = newTask.querySelector('.elements__foto');
+  linkElement.setAttribute('src', linkPlace);
+  linkElement.setAttribute('alt', namePlace);
+
+  elementsSection.append(newTask);
+}
+
+buttonAdd.addEventListener('click', function() {
+  const nameValue = inputNamePlace.value;
+  const linkValue = inputLinkPlace.value;
+  const newTask = taskTemplate.content.querySelector('.elements__element').cloneNode('true');
+
+  const nameElement = newTask.querySelector('.elements__foto-name');
+  nameElement.textContent = nameValue;
+  const linkElement = newTask.querySelector('.elements__foto');
+  linkElement.setAttribute('src', linkValue);
+  linkElement.setAttribute('alt', nameValue);
+
+  elementsSection.prepend(newTask);
+});
+
+//Функционал работы кнопки добавления карточек
+function addSubmitHandler(evt) {
+  evt.preventDefault();
+  popupPlaceStatus();
+}
+
+popupPlace.addEventListener('submit', addSubmitHandler);
+//функционал изменения цвета лайка
+function testing () {
+const activeLike = document.querySelectorAll(".elements__like");
+activeLike.forEach(function (evt) {
+  evt.addEventListener("click", function () {
+    evt.classList.toggle("elements__active-like");;
+  });
+});
+}
+testing();
+
+//функционал удаления карточки
+/*const buttonDelete = taskTemplate.content.querySelector('.elements__delete');
+console.log(buttonDelete);
+function elementDelete() {
+
+}*/
